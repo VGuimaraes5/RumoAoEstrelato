2a53ea251064324a6d525cd6dae39f80f4a6a3fa
Vinicius Guimarães <vinicius.guimaraes5@fatec.sp.gov.br>
2021-06-08 20:52:40 -0300

    Adiciona primeira cena de dialogo

---
+++ Assets/Scripts/Management/Scene1_0.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scene1_0 : MonoBehaviour
+{
+
+    [SerializeField]
+    private string nextScene;
+    [SerializeField]
+    private SceneLoader sceneLoader;
+
+    [SerializeField]
+    private Fungus.Flowchart flowchart;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (flowchart.GetBooleanVariable("SceneEnd"))
+        {
+            StartCoroutine(FinishLevel());
+        }
+    }
+
+    IEnumerator FinishLevel()
+    {
+        yield return new WaitForSeconds(2f);
+        sceneLoader.Transition(nextScene);
+    }
+}
